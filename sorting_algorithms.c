8511feaffdbd5a5b7b90ea2a1ec0443839dec32d
Arthur <arthurtavares3109@gmail.com>
2026-02-25 15:39:22 -0300

    implementando selection sort

--- sorting_algorithms.c
+++ sorting_algorithms.c
@@ -74,3 +74,14 @@ void bubbleSort(int arr[], int n) {
                 arr[j + 1] = temp;
             }
 }
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_idx]) min_idx = j;
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
